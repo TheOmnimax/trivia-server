1ca49bdae5dbce098fee4b1f374ed06ebcb9fd7a
TheOmnimax <max@maxshaberman.com>
2022-08-20 20:58:13 -0400

    Moved checkin to its own outer function

--- routers/play/playing.py
+++ routers/play/playing.py
@@ -22,6 +22,73 @@ def _getGame(room_code: str, mem_store: GcloudMemoryStorage) -> tuple[str, Trivi
   game = mem_store.get(kind='trivia_game', id=game_id, data_type=TriviaGame)
   return (game_id, game)
 
+def completionCheck(room_code: str, player_id: str, mem_store: GcloudMemoryStorage):
+  game_id, game = _getGame(room_code=room_code, mem_store=mem_store)
+  def nr():
+    tg_services.nextRound(game_id=game_id, player_ids=players.keys(), transaction=mem_store.transaction)
+  
+  def completeRound(game: TriviaGame):
+    game.round_complete = True
+    tg_services.completeRound(
+        game,
+        player_data=players,
+        completionFunction=Timer(1.0, nr).start # Wait a couple of seconds before starting the next round
+        )
+  
+  def predicate(game: TriviaGame): # True if ready to begin calculations for the next round. Using "round_complete" to make sure they do not happen multiple times
+    # print('Predicate')
+    # print('Round complete:', game.round_complete)
+    # print('From function:', tg_services.roundComplete(player_data=players, winning_time=game.winning_time))
+    return (not game.round_complete) and tg_services.roundComplete(player_data=players, winning_time=game.winning_time)
+  players = mem_store.getMulti(
+    kind='player',
+    ids=game.players,
+    data_type=TriviaPlayer
+    )
+  mem_store.transaction(kind='trivia_game', id=game_id, new_val_func=completeRound, predicate=predicate)
+
+  current_question = tg_services.getQuestion(game)
+  if game.round_complete:
+    winners_ids = tg_services.getRoundResults(game).winners
+    winner_names = [players[p].name for p in winners_ids]
+    is_winner = player_id in winners_ids
+
+    if game.game_complete: # TODO: Add delay before completing game
+      return GameCompleteResponse(
+        correct=tg_services.getCorrectValue(game),
+        winners=winner_names,
+        is_winner=is_winner,
+      )
+    else: # The round is complete, but not the game, so can display info about the round, including who won
+      return StillPlaying(
+        question=current_question.label,
+        choices=current_question.choices,
+        player_complete=True,
+        round_complete=True,
+        game_complete=False,
+        correct=tg_services.getCorrectValue(game),
+        winners=winner_names,
+        is_winner=is_winner,
+      )
+  elif players[player_id].selected_choice > -1:
+    return StillPlaying(
+      question=current_question.label,
+      choices=current_question.choices,
+      player_complete=True,
+      round_complete=False,
+      game_complete=False
+    )
+  else:
+    return StillPlaying(
+      question=current_question.label,
+      choices=current_question.choices,
+      player_complete=False,
+      round_complete=False,
+      game_complete=False
+    )
+
+# ROUTERS
+
 @router.post('/start-game')
 async def startGame(data: AdminSchema, mem_store: GcloudMemoryStorage = Depends(dependencies.getMemoryStorage)):
 
@@ -70,65 +137,8 @@ async def playerCheckin(data: PlayerCheckinSchema, mem_store: GcloudMemoryStorag
   if not (player.completed_round or tg_services.roundComplete(player_data=players, winning_time=game.winning_time)): # Only add time if player is not done
     mem_store.transaction(id=data.player_id, kind='player', new_val_func=pc)
 
+  return completionCheck(room_code=data.room_code, player_id=data.player_id, mem_store=mem_store)
 
-  current_question = tg_services.getQuestion(game)
-  def nr():
-    tg_services.nextRound(game_id=game_id, player_ids=players.keys(), transaction=mem_store.transaction)
-  
-  def completeRound(game: TriviaGame):
-    game.round_complete = True
-    tg_services.completeRound(
-        game,
-        player_data=players,
-        completionFunction=Timer(1.0, nr).start # Wait a couple of seconds before starting the next round
-        )
-  
-  def predicate(game: TriviaGame): # True if ready to begin calculations for the next round. Using "round_complete" to make sure they do not happen multiple times
-    # print('Predicate')
-    # print('Round complete:', game.round_complete)
-    # print('From function:', tg_services.roundComplete(player_data=players, winning_time=game.winning_time))
-    return (not game.round_complete) and tg_services.roundComplete(player_data=players, winning_time=game.winning_time)
-
-  mem_store.transaction(kind='trivia_game', id=game_id, new_val_func=completeRound, predicate=predicate)
-  
-  if game.round_complete:
-    winners_ids = tg_services.getRoundResults(game).winners
-    winner_names = [players[p].name for p in winners_ids]
-    is_winner = data.player_id in winners_ids
-
-    if game.game_complete: # TODO: Add delay before completing game
-      return GameCompleteResponse(
-        correct=tg_services.getCorrectValue(game),
-        winners=winner_names,
-        is_winner=is_winner,
-      )
-    else: # The round is complete, but not the game, so can display info about the round, including who won
-      return StillPlaying(
-        question=current_question.label,
-        choices=current_question.choices,
-        player_complete=True,
-        round_complete=True,
-        game_complete=False,
-        correct=tg_services.getCorrectValue(game),
-        winners=winner_names,
-        is_winner=is_winner,
-      )
-  elif players[data.player_id].selected_choice > -1:
-    return StillPlaying(
-      question=current_question.label,
-      choices=current_question.choices,
-      player_complete=True,
-      round_complete=False,
-      game_complete=False
-    )
-  else:
-    return StillPlaying(
-      question=current_question.label,
-      choices=current_question.choices,
-      player_complete=False,
-      round_complete=False,
-      game_complete=False
-    )
 
 @router.post('/answer-question')
 async def answerQuestion(data: AnswerQuestion, mem_store: GcloudMemoryStorage = Depends(dependencies.getMemoryStorage)):
