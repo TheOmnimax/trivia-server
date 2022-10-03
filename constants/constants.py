15e87d9d1a01cb74aa3bd92d44e4c51fbf21ea00
TheOmnimax <max@maxshaberman.com>
2022-10-03 00:29:50 -0400

    Moved origins to own file

---
+++ constants/constants.py
@@ -0,0 +1,12 @@
+
+
+origins = [
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:62229',
+    'https://localhost:62229',
+    'https://max-trivia.web.app',
+    'http://max-trivia.web.app',
+    'https://trivia-question-manager.web.app',
+    'http://trivia-question-manager.web.app'
+]
